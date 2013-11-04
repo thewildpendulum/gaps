06b9ac101a17641ed2e0b4ba260163f9153615a7
thewildpendulum <trhemphill@gmail.com>
2013-11-04 00:59:57 -0600

    init

---
+++ app/templates/js/_controllers.js
@@ -0,0 +1,7 @@
+angular.module('<%= appName %>.controllers', [])
+    .controller('MainCtrl', ['$scope', function ($scope) {
+        $scope.status = "It works!";
+    }])
+    .controller('ViewCtrl', ['$scope', function ($scope) {
+        $scope.status = "Also totally works!";
+    }]);
